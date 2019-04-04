10372587a9d91d425c81744d84246c758b06b441
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2019-04-04 12:19:35 -0400

    got the card flip to work

--- client/src/pages/YogaPose.js
+++ client/src/pages/YogaPose.js
@@ -20,8 +20,32 @@ class YogaPose extends Component {
             className={`card ${this.state.isFlipped ? 'is-flipped' : ''}`}
             onClick={() => this.setState({ isFlipped: !this.state.isFlipped })}
           >
-            <div className="card__face card__face--front">front stuff goes here</div>
-            <div className="card__face card__face--back">back stuff goes here</div>
+            <div className="card__face card__face--front">
+              <img
+                src={require('../images/yoga_stock_img.jpg')}
+                // className="card__face card__face--front"
+                alt="tree-pose-image"
+              />
+            </div>
+            <div className="detail-card-outline card__face card__face--back">
+              <ol className="pose-details-list list-group">
+                <li className="list-group-item">Sanskrit Name</li>
+                <li className="list-group-item">English Name</li>
+                <li className="list-group-item">Position Type</li>
+                <li className="list-group-item">
+                  <h6>Steps to Get into the Pose</h6>
+                </li>
+                <li className="list-group-item">Step 1</li>
+                <li className="list-group-item">Step 2</li>
+                <li className="list-group-item">Step 3</li>
+                <li className="list-group-item">Step 4</li>
+                <li className="list-group-item">Step 5</li>
+                <li className="list-group-item">
+                  <b>Description:</b> Handstand half moon pose half boat pose eight angle pose child's pose frog pose
+                  bird of paradise pose crab pose staff pose flying crow pose.
+                </li>
+              </ol>
+            </div>
           </div>
         </div>
       </section>
@@ -39,10 +63,6 @@ const temp = () => {
           src={require('../images/yoga_stock_img.jpg')}
           className="card__face card__face--front"
           alt="tree-pose-image"
-          // style={{ isFlipped: this.state.isFlipped }}
-          // onClick={() => this.setState({ isFlipped: !this.state.isFlipped })}
-
-          //pose-detail-img
         />
       </section>
 
@@ -53,9 +73,6 @@ const temp = () => {
           <li className="list-group-item">Sanskrit Name</li>
           <li className="list-group-item">English Name</li>
           <li className="list-group-item">Position Type</li>
-          {/* </ol>
-
-      <ol className="pose-details-list list-group"> */}
           <li className="list-group-item">
             <h6>Steps to Get into the Pose</h6>
           </li>
