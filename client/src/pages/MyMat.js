fdfe81dc1a8324148d90576e3cf16527179eac36
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2019-04-15 10:21:44 -0400

    working on styling and html of the my mat instructor profile section. added new pic

--- client/src/pages/MyMat.js
+++ client/src/pages/MyMat.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
-import instructorImg from '../images/Instructor.jpeg'
+import instructorImg from '../images/profile_pic.jpg'
 
 class MyMat extends Component {
   render() {
@@ -16,14 +16,20 @@ class MyMat extends Component {
             <li>
               <article>
                 <aside>
-                  <img src={instructorImg} /> <p>Image of Instructor and className</p>
+                  <img src={instructorImg} alt="instructor-image" />
+                  <header>
+                    <h3>Course Name</h3>
+                  </header>
                 </aside>
                 <section>
-                  Handstand half moon pose half boat pose eight angle pose child's pose frog pose bird of paradise pose
-                  crab pose staff pose flying crow pose sage koundinya i pose one-legged king pigeon pose ii eagle pose
-                  cow face pose marichi's pose ii big toe pose gate pose full boat pose marichi's pose iii, seated twist
-                  side crane pose, side crow pose yoga plank pose shoulderstand, supported shoulderstand locust pose
-                  easy pose knees to chest pose upward facing dog pose chair pose warrior ii pose hero pose.
+                  <p>
+                    Handstand half moon pose half boat pose eight angle pose child's pose frog pose bird of paradise
+                    pose crab pose staff pose flying crow pose sage koundinya i pose one-legged king pigeon pose ii
+                    eagle pose cow face pose marichi's pose ii big toe pose gate pose full boat pose marichi's pose iii,
+                    seated twist side crane pose, side crow pose yoga plank pose shoulderstand, supported shoulderstand
+                    locust pose easy pose knees to chest pose upward facing dog pose chair pose warrior ii pose hero
+                    pose.
+                  </p>
                 </section>
               </article>
             </li>
